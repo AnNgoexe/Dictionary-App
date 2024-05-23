768511fcf463db3cfb4cb4380ee74334399db2d9
AnNgoexe <quocan2004tp@gmail.com>
2024-05-24 00:13:29 +0700

    start doing game

---
+++ src/main/java/com/example/dictionary/GameController.java
@@ -0,0 +1,101 @@
+package com.example.dictionary;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class GameController {
+    public TextArea exerciseTextArea;
+    
+    public Label scoreLabel;
+    public ProgressBar timeProgressBar;
+    public Button nextButton;
+
+    @FXML
+    private Button optionCButton;
+
+    @FXML
+    private Button optionAButton;
+
+    @FXML
+    private Button optionBButton;
+
+    @FXML
+    private Button optionDButton;
+
+    @FXML
+    private Button muteButton;
+
+    @FXML
+    private Button unMuteButton;
+
+    @FXML
+    private ImageView secondHeart;
+
+    @FXML
+    private ImageView thirdHeart;
+
+    @FXML
+    private ImageView firstHeart;
+
+    @FXML
+    private Button moreHeartHelpButton;
+
+    @FXML
+    private Button fiftyFiftyHelpButton;
+
+    @FXML
+    private Button moreTimeHelpButton;
+
+    @FXML
+    public void moreHeartHelp(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void chooseCOption(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void chooseBOption(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void chooseAOption(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void chooseDOption(ActionEvent actionEvent) {
+
+    }
+
+    public void moreTimeHelp(ActionEvent actionEvent) {
+
+    }
+
+    public void fiftyFiftyHelp(ActionEvent actionEvent) {
+        
+    }
+
+    public void unMute(ActionEvent actionEvent) {
+        
+    }
+
+    public void mute(ActionEvent actionEvent) {
+        
+    }
+
+    public void editTimeProgress(MouseEvent mouseEvent) {
+    }
+
+    public void nextExercise(ActionEvent actionEvent) {
+    }
+}
